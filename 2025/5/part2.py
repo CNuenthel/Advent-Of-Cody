a3ba2c6e813faddb98e916e0e21f8e73df21fce6
CNuenthel <cnuenthel@gmail.com>
2025-12-05 16:02:52 -0600

    Notes to help me understand my tortured mind.

--- 2025/5/part2.py
+++ 2025/5/part2.py
@@ -1,5 +1,9 @@
 
 def merge_range(ranges: list, target_range: tuple):
+    """
+    Takes a range, and continuously merges ranges in a list if those ranges overlap with target range. Once
+    an overlap is not detected, returns the merged range, and the remaining ranges for follow-up merging.
+    """
     while True:
         if not ranges:
             break
