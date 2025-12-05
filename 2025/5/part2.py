b2da95c6f0764d026fa9f44df8ac31c3b6aec183
CNuenthel <cnuenthel@gmail.com>
2025-12-05 13:56:13 -0600

    Day5 Part 2 completed

--- 2025/5/part2.py
+++ 2025/5/part2.py
@@ -1,17 +1,50 @@
 
+def merge_range(ranges: list, target_range: tuple):
+    while True:
+        if not ranges:
+            break
+        ingredient_range = ranges.pop(0)
+        if target_range[0] <= ingredient_range[0] <= target_range[1] or target_range[0] <= ingredient_range[1] <= target_range[1]:
+            target_range = (min(ingredient_range+target_range), max(ingredient_range+target_range))
+        else:
+            ranges.insert(0, ingredient_range)
+            break
+
+    return target_range, ranges
 
 def main():
     with open("input.txt", "r") as f:
         data = f.readlines()
+
     data = [item.replace("\n", "") for item in data]
-
     split_index = data.index("")
-    ranges, ingredients = data[:split_index], data[split_index+1:]
+    ranges, ingredients = data[:split_index], data[split_index + 1:]
+    ranges = sorted([(int(num.split("-")[0]), int(num.split("-")[1])) for num in ranges])
 
-    ranges = [(int(num.split("-")[0]), int(num.split("-")[1])) for num in ranges]
-    ingredients = [int(num) for num in ingredients]
+    range_count = len(ranges)
 
+    while True:
+        merged_ranges = []
 
+        while ranges:
+            t = ranges.pop(0)
+
+            if not ranges:
+                merged_ranges.append(t)
+                break
+
+            mr, ranges = merge_range(ranges, t)
+            merged_ranges.append(mr)
+
+        ranges = merged_ranges
+
+        if len(merged_ranges) == range_count:
+            break
+        else:
+            range_count = len(merged_ranges)
+
+    fresh_ingredients = sum([r[1] - r[0] + 1 for r in ranges])
+    print(f"Fresh Ingredients: {fresh_ingredients}")
 
 if __name__ == "__main__":
     main()
