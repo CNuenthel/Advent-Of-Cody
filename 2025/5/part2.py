8f6285711fa358c00ea1988d5c4189f02772c128
CNuenthel <cnuenthel@gmail.com>
2025-12-05 14:01:21 -0600

    Fixed merge_range function to actually work as intended

--- 2025/5/part2.py
+++ 2025/5/part2.py
@@ -4,12 +4,14 @@ def merge_range(ranges: list, target_range: tuple):
         if not ranges:
             break
         ingredient_range = ranges.pop(0)
-        if target_range[0] <= ingredient_range[0] <= target_range[1] or target_range[0] <= ingredient_range[1] <= target_range[1]:
+        if (target_range[0] <= ingredient_range[0] <= target_range[1]
+                or target_range[0] <= ingredient_range[1] <= target_range[1]
+                or ingredient_range[0] <= target_range[0] <= ingredient_range[1]
+                or ingredient_range[0] <= target_range[1] <= ingredient_range[1]):
             target_range = (min(ingredient_range+target_range), max(ingredient_range+target_range))
         else:
             ranges.insert(0, ingredient_range)
             break
-
     return target_range, ranges
 
 def main():
@@ -21,29 +23,16 @@ def main():
     ranges, ingredients = data[:split_index], data[split_index + 1:]
     ranges = sorted([(int(num.split("-")[0]), int(num.split("-")[1])) for num in ranges])
 
-    range_count = len(ranges)
-
-    while True:
-        merged_ranges = []
-
-        while ranges:
-            t = ranges.pop(0)
-
-            if not ranges:
-                merged_ranges.append(t)
-                break
-
-            mr, ranges = merge_range(ranges, t)
-            merged_ranges.append(mr)
-
-        ranges = merged_ranges
-
-        if len(merged_ranges) == range_count:
+    merged_ranges = []
+    while ranges:
+        t = ranges.pop(0)
+        if not ranges:
+            merged_ranges.append(t)
             break
-        else:
-            range_count = len(merged_ranges)
+        mr, ranges = merge_range(ranges, t)
+        merged_ranges.append(mr)
 
-    fresh_ingredients = sum([r[1] - r[0] + 1 for r in ranges])
+    fresh_ingredients = sum([r[1] - r[0] + 1 for r in merged_ranges])
     print(f"Fresh Ingredients: {fresh_ingredients}")
 
 if __name__ == "__main__":
