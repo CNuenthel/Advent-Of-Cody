d46c295a830013d16e29dc70ea3e01e3004bcb4b
synthwave <cnuenthel@gmail.com>
2025-12-07 13:38:09 -0600

    Day7 part 2 done

--- 2025/7/part1.py
+++ 2025/7/part1.py
@@ -1,26 +1,21 @@
 
 with open("input.txt", "r") as f:
-    lines = f.readlines()
-
-lines = [lines.replace("\n", "") for lines in lines]
-lines = [[char for char in line] for line in lines]
+    data = f.readlines()
+lines = [[char for char in line.replace("\n", "")] for line in data]
 
 count = 0
 for i, line in enumerate(lines):
     for j, char in enumerate(line):
         if char == "S":
             lines[i+1][j] = "|"
-        if char == "^":
+        elif char == "^":
             if lines[i-1][j] == "|":
                 lines[i][j-1] = "|"
                 lines[i][j+1] = "|"
                 count += 1
-        else:
+        elif char == ".":
             if lines[i-1][j] == "|":
                 lines[i][j] = "|"
 
-for line in lines:
-    print(line)
-
-print("")
 print(count)
+
---
+++ 2025/7/part2.py
@@ -0,0 +1,42 @@
+
+with open("input.txt", "r") as f:
+    data = f.readlines()
+lines = [[char for char in line.replace("\n", "")] for line in data]
+
+rows = len(lines)
+cols = len(lines[0])
+ways = [[0]*cols for _ in range(rows)]
+
+for x, ch in enumerate(lines[0]):
+    if ch == 'S':
+        ways[0][x] = 1
+
+for y in range(rows):
+    for x in range(cols):
+        w = ways[y][x]
+        if w == 0:
+            continue
+
+        cell = lines[y][x]
+
+        if cell == '|' or cell == 'S' or cell == '.':
+            if y+1 < rows:
+                ways[y+1][x] += w
+
+        if cell == '^':
+            if y+1 < rows and x-1 >= 0:
+                ways[y+1][x-1] += w
+            if y+1 < rows and x+1 < cols:
+                ways[y+1][x+1] += w
+
+def display_ways(ways):
+    for row in ways:
+        formatted = [
+            f"{val:4d}" if val != 0 else "   ."
+            for val in row
+        ]
+        print(" ".join(formatted))
+
+display_ways(ways)
+total_paths = sum(ways[rows-1])
+print(total_paths)
