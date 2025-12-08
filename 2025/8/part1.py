93dd436fbd5a0b2374c04694bbaed1b8368bb0b7
CNuenthel <cnuenthel@gmail.com>
2025-12-08 12:07:36 -0600

    Still spitballing part 1

--- 2025/8/part1.py
+++ 2025/8/part1.py
@@ -1,50 +1,108 @@
 import math
 import uuid
 
-with open("input.txt", "r") as f:
-    lines = f.readlines()
+class Grid:
+    def __init__(self):
+        self.id = str(uuid.uuid4())[:4]
+        self.circuits = []
 
-lines = [line.replace("\n", "") for line in lines]
+    def __repr__(self):
+        return self.id
 
-class Coordinate:
+    def __len__(self):
+        return len(self.circuits)
+
+    def add_circuit(self, circuit: "Circuit"):
+        self.circuits.append(circuit)
+
+    def new_circuit(self, junction1: "Junction", junction2: "Junction"):
+        circuit = Circuit(grid=self)
+        circuit.add_junction(junction1)
+        circuit.add_junction(junction2)
+        self.circuits.append(circuit)
+        return circuit
+
+    def query_circuits(self, junction: "Junction"):
+        for c in self.circuits:
+            if c.query_junctions(junction):
+                return c
+        return None
+
+class Circuit:
+    def __init__(self, grid: "Grid"):
+        self.id = str(uuid.uuid4())[:4]
+        self.grid = grid
+        self.junctions = []
+
+    def __repr__(self):
+        return self.id
+
+    def __len__(self):
+        return len(self.junctions)
+
+    def add_junction(self, junction: "Junction"):
+        self.junctions.append(junction)
+
+    def query_junctions(self, junction: "Junction"):
+        for j in self.junctions:
+            if junction == j:
+                return True
+        return False
+
+class Junction:
     def __init__(self, x, y, z):
-        self.coord = (int(x),int(y),int(z))
+        self.id = str(uuid.uuid4())[:4]
+        self.coord = (int(x), int(y), int(z))
         self.connection = None
         self.connection_distance = None
-        self.circuit_id = None
+        self.circuit = None
+        self.grid = None
 
-    def check_other_coordinate_distance(self, other: "Coordinate"):
+    def check_other_coordinate_distance(self, other: "Junction") -> float:
         return math.dist(self.coord, other.coord)
 
-    def set_connection(self, other: "Coordinate"):
-        if not other.circuit_id and not self.circuit_id:
-            self.circuit_id = uuid.uuid4()
-
-        self.circuit_id = connection_id
+    def set_connection(self, other: "Junction", connection_distance: float, circuit: "Circuit", grid: "Grid"):
+        self.connection = other
+        self.connection_distance = connection_distance
+        self.circuit = circuit
+        self.grid = grid
+        print(f"Connecting {self} to {other}")
 
     def __eq__(self, other):
         return self.coord == other.coord
 
     def __repr__(self):
-        return str(self.coord)
+        return self.id
 
-points = [Coordinate(*line.split(",")) for line in lines]
+def main():
+    with open("input.txt", "r") as f:
+        lines = f.readlines()
 
-for point in points:
-    for p in points:
-        if p == point:
-            continue
+    lines = [line.replace("\n", "") for line in lines]
+    junctions = [Junction(*line.split(",")) for line in lines]
 
-        shortest_coord = p
-        shortest_dist = 999999999
+    grid = Grid()
 
-        dist = point.check_other_coordinate_distance(p)
-        if dist < shortest_dist:
-            shortest_dist = dist
-            shortest_coord = p
+    for junction in junctions:
+        if not junction.connection:
+            distances = [junction.check_other_coordinate_distance(j) for j in junctions if j != junction]
+            shortest_dist: float = min(distances)
+            shortest_dist_index: int = distances.index(shortest_dist) + 1
+            closest_junc: Junction = junctions[shortest_dist_index]
 
-    point.set_connection
+            if closest_junc.connection:
+                junction.set_connection(other=closest_junc, connection_distance=shortest_dist, circuit=closest_junc.circuit, grid=grid)
+                closest_junc.circuit.add_junction(junction)
+            else:
+                circuit = grid.new_circuit(junction, closest_junc)
+                junction.set_connection(other=closest_junc, connection_distance=shortest_dist, circuit=circuit, grid=grid)
+                closest_junc.set_connection(other=junction, connection_distance=shortest_dist, circuit=circuit, grid=grid)
 
+    return grid
+
+
+if __name__ == "__main__":
+    g = main()
 
 
 
